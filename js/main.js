908c7cd7d47eb2e37f7cdc96593090baf733ba1e
jsap1904 <sapinoso.j@gmail.com>
2017-04-04 20:17:14 -0700

    update

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// var heads = 1
+// var tails = 0
+// var userPick = userInput.value
+// var comPick = (Math.random() * 2)
+
+
+// function coinToss {
+// 	console.log(Math.random() * 2)
+// if (userPick &&== heads)
+// 	console.log("")
+// 		
+
+function coinToss {
+	var chance = Math.floor(Math.random() * 2)
+	var coinFlip = document.getElementById("coinFlipper")
+
+	if (chance ==0) {
+		coinFlipper.src = img 1
+
+	} else {
+		coinFlipper.src = img 2
+	}
+}
