e849d238fbf68aef56d7e7e04200a320751e376a
jsap1904 <sapinoso.j@gmail.com>
2017-04-07 17:04:37 -0700

    update

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 
-function coinToss {
+function coinToss () {
 	var chance = Math.floor(Math.random() * 2)
-	var coinFlip = document.getElementById("coinFlipper").value
+	var coinFlip = document.getElementById("coinFlipper")
+	
+	
+	if (chance == 0) {
+		alert("heads wins")
+			coinFlip.src = "img/heads.png"
+} 
 
-	if (chance ==0) {
-		coinFlipper.src = img 1
-
-	} else {
-		coinFlipper.src = img 2
+	else {
+		alert("tails wins")
+			coinFlip.src = "img/tails.png"
 	}
 }
+
